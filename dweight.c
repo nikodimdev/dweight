74399bb78fca460bbeafcef379451eba4e35f02f
nikodimdev <nikodimkim@gmail.com>
2025-06-19 04:22:28 +0300

    Update dweight.c

--- dweight.c
+++ dweight.c
@@ -5,7 +5,6 @@ main (void)
 {
   int height, length, width, volume, weight;
 
-
   printf ("Введите высоту коробки: ");
   scanf ("%d", &height);
 
@@ -16,25 +15,11 @@ main (void)
   scanf ("%d", &width);
 
 
-
-
   volume = height * length * width;
   weight = (volume + 165) / 166;
 
-  printf("Габаритный размер: %dx%dx%d\n", height, length, width);
-  printf("Объём: %d\n", volume);
-  printf("Размерный вес: %d\n", (volume + 165) / 166);
+  printf ("Габаритный размер: %dx%dx%d\n", height, length, width);
+  printf ("Объём: %d\n", volume);
+  printf ("Размерный вес: %d\n", (volume + 165) / 166);
   return 0;
 }
-
-
-
-/*
-
-
-Задача из книги C Programming: A Modern Approach" (2nd Edition) by K.N. King
-Цель: написать программу которая считает габаритного вес коробки
-
-
-
-*/
